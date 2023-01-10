95603eaf623e7e7333e4cf57641991d63305b35f
Elie Hymowitz <ehymowitz@gmail.com>
2023-01-10 13:02:34 +1100

    feat: time picker on android

--- components/inputs/timePicker.tsx
+++ components/inputs/timePicker.tsx
@@ -1,11 +1,8 @@
-import { TextField } from "@mui/material";
-import {
-  ClockPicker,
-  LocalizationProvider,
-  StaticTimePicker,
-} from "@mui/x-date-pickers";
+import { ClockPicker, LocalizationProvider } from "@mui/x-date-pickers/";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Dayjs } from "dayjs";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import dayjs, { Dayjs } from "dayjs";
+import * as Device from "expo-device";
 import React, { useState } from "react";
 import PrimaryButton from "../buttons/primaryButton";
 import TitleText from "../typography/titleText";
@@ -17,26 +14,46 @@ interface TimePickerProps {
 
 const TimePicker = ({ time, setTime }: TimePickerProps) => {
   const [viewHours, setViewHours] = useState(true);
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <ClockPicker
-        date={time}
-        onChange={(newValue) => {
-          setTime(newValue || undefined);
-          if (viewHours) {
-            setViewHours(false);
-          }
-        }}
-        views={["hours", "minutes"]}
-        view={viewHours ? "hours" : "minutes"}
-      />
-      <TitleText>{time?.format("h:mm a")}</TitleText>
 
-      <PrimaryButton
-        onPress={() => setViewHours(!viewHours)}
-        title="Hours/Minutes"
-      />
-    </LocalizationProvider>
+  const handleChangeDevice = (time?: Date) => {
+    setTime(dayjs(time));
+  };
+
+  const showTime = () => {
+    DateTimePickerAndroid.open({
+      value: (time || dayjs()).toDate(),
+      onChange: (_e, date) => handleChangeDevice(date),
+      mode: "time",
+      is24Hour: true,
+    });
+  };
+
+  return (
+    <>
+      <TitleText>Alarm Time: {time?.format("h:mm a")}</TitleText>
+      {Device.brand ? (
+        <PrimaryButton title="Select Time" onPress={showTime} />
+      ) : (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <ClockPicker
+            date={time}
+            onChange={(newValue) => {
+              setTime(newValue || undefined);
+              if (viewHours) {
+                setViewHours(false);
+              }
+            }}
+            views={["hours", "minutes"]}
+            view={viewHours ? "hours" : "minutes"}
+          />
+
+          <PrimaryButton
+            onPress={() => setViewHours(!viewHours)}
+            title="Hours/Minutes"
+          />
+        </LocalizationProvider>
+      )}
+    </>
   );
 };
 
